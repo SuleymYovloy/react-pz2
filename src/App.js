3a53b5065c6487d62bf8ff40292a160f63ac4de8
Suleym <suleymevlo81@gmail.com>
2023-05-19 00:52:26 +0300

    Сделал выбор типа и размера пицц

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import './scss/app.scss';
 import Header from './components/Header';
 import Sort from './components/Sort';
 import Categories from './components/Categories';
 import PizzaBlock from './components/PizzaBlock';
+import pizzas from './assets/pizzas.json';
+
+import './scss/app.scss';
 
 function App() {
   return (
@@ -17,8 +19,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="hello" price="500" />
-            <PizzaBlock />
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
